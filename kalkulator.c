4591faf1f6393569c088ea7f50b58062777af0a0
Miku91 <krystian5162@op.pl>
2020-01-05 13:53:57 +0100

    Dodanie 'if' w 'kalkulator.c'

--- kalkulator.c
+++ kalkulator.c
@@ -4,12 +4,18 @@
 float main()
 {
     float a, b;
+    int op;
     puts("Podaj 1 liczbe");
     scanf(" &f", a);
     puts("Podaj 2 liczbe");
     scanf(" &f", b);
     printf("Co chcesz zrobić? \n");
     printf("(dodawanie-1 | odejmowanie-2 | mnozenie-3 | dzielenie-4");
+    scanf("%d",&op);
+    if(op==1)
+    {
+    dodawanie.h
+    }
  return 0;
 }
 
